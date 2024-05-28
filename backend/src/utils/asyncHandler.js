e4074d142d38790a9289b8cb7e6d53c67f0d7e12
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-05-28 22:41:46 +0530

    add requestHandler function Util seperately

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+            console.log(error);
+            next(error);
+        });
+    }};
