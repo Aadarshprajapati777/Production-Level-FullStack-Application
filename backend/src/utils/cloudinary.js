3073cc9ba75d85368839239d6e47680744cc872b
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-05-30 10:53:41 +0530

    feat: file upload with cloudinary

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localfilepath) => {
+    try {
+        if(!localfilepath){
+            throw new Error("Please provide the file path")
+        }
+        const result = await cloudinary.uploader.upload(localfilepath, {
+
+            resource_type: "auto"
+        });
+
+        fs.unlinkSync(localfilepath);
+        console.log("fill has been uploaded",result.url);
+        return result.url;
+    }
+        catch(err){
+            console.log(err);
+        }
+
+
+    }
